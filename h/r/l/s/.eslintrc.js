d3c734f7add6906a911be23c31d7291a7883d2f0
TYleRW83811 <swirl_09_sours@icloud.com>
2025-03-11 14:32:52 +0000

    Add comments to explain complex logic in the file

--- h/r/l/s/.eslintrc.js
+++ h/r/l/s/.eslintrc.js
@@ -17,7 +17,6 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2022,
     sourceType: 'module',
-  },
   settings: {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
