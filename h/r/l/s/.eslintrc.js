74b6b0e4ecb0b9f62b3f3797f439622597db6858
TYleRW83811 <swirl_09_sours@icloud.com>
2025-03-13 21:03:49 +0000

    Fix a typo in the error message in the file

--- h/r/l/s/.eslintrc.js
+++ h/r/l/s/.eslintrc.js
@@ -21,7 +21,6 @@ module.exports = {
     'import/resolver': {
       // See https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-575727774 for line below
       node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx'],
         moduleDirectory: ['node_modules', 'src/'],
       },
       webpack: {
